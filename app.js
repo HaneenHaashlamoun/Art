de9cb72c0147cbe1f993c014d17838a2d55869f1
HaneenHatem89 <>
2021-06-16 19:56:07 +0000

    update alerts

--- app.js
+++ app.js
@@ -1,55 +1,38 @@
-alert("Welcome to the museum ..!")
-var name = prompt("what is your name artist :) ?")
+
+var name = prompt("what is your name ?")
+
+alert("Welcome to the museum ..!" + name)
 
 var fav = prompt("what is your favourite art subject of these (modern, archi, abstract) " + name + "?")
 
-
-
 if (fav == "modern") {
 
-  var answer = prompt(name + " would you like to see this type ?")
+  var answer = prompt(name + " who is your favourite artist ?")
+  document.write("<div>" +
+    "<img src='https://cdn-anmal.nitrocdn.com/vJgtiwkCsLhvhzZzqhfdngZGCtTVAAGD/assets/static/optimized/rev-25bc62b/wp-content/uploads/2020/03/modern-paintings-art-4-768x768.jpg'/>" +
+    "<h4>" + "Modern Art" + "</h4>" +
+    "</div>")
 
-  if (answer == "yes") {
-
-    document.write("<div>" +
-      "<img src='https://cdn-anmal.nitrocdn.com/vJgtiwkCsLhvhzZzqhfdngZGCtTVAAGD/assets/static/optimized/rev-25bc62b/wp-content/uploads/2020/03/modern-paintings-art-4-768x768.jpg'/>" +
-      "<h4>" + "Modern Art" + "</h4>" +
-      "</div>")
-
-
-  }
-  else {
-    alert("Have a nice day ! ")
-  }
+  document.write("Welcome to Modern art " + name + " fan of " + answer)
 }
 else if (fav == "archi") {
-  var answer = prompt(name + " would you like to see this type ?")
+  var answer = prompt(name + " who is your favourite artist ?")
 
-  if (answer == "yes") {
-
-    document.write("<div>" +
-      "<img src='https://onehundrededition.com/wp-content/uploads/2018/12/PARIS-in-100%C2%BA-The-10-Top-Contemporary-Art-Galleries-8.jpg'/>" +
-      "<h4>" + "Architecture" + "</h4>" +
-      "</div>")
-  }
-  else {
-    alert("Have a nice day ! ")
-  }
+  document.write("<div>" +
+    "<img src='https://onehundrededition.com/wp-content/uploads/2018/12/PARIS-in-100%C2%BA-The-10-Top-Contemporary-Art-Galleries-8.jpg'/>" +
+    "<h4>" + "Architecture" + "</h4>" +
+    "</div>")
+  document.write("Welcome to Architecture art " + name + " fan of " + answer)
 
 }
 else if (fav == "abstract") {
-  var answer = prompt(name + " would you like to see this type ?")
+  var answer = prompt(name + " who is your favourite artist ?")
 
-  if (answer == "yes") {
-
-    document.write("<div>" +
+   document.write("<div>" +
       "<img src='https://onehundrededition.com/wp-content/uploads/2018/12/PARIS-in-100%C2%BA-The-10-Top-Contemporary-Art-Galleries-10.jpg'/>" +
       "<h4>" + "Abstractical" + "</h4>" +
       "</div>")
-  }
-  else {
-    alert("Have a nice day ! ")
-  }
+      document.write("Welcome to Abstractical art " + name + " fan of " + answer)
 
 }
 else {
