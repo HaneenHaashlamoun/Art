36c8a9de0e5fca399550f6070086ec7dd2a2bc68
HaneenHatem89 <>
2021-06-17 13:36:28 +0000

    adding loops

--- app.js
+++ app.js
@@ -4,23 +4,28 @@ var name = prompt("what is your name ?")
 alert("Welcome to the museum ..!" + name)
 
 var fav = prompt("what is your favourite art subject of these (modern, archi, abstract) " + name + "?")
-var answer = prompt(name + " who is your favourite artist ?")
+
+while (fav !== "modern" && fav !== "archi" && fav !== "abstract") {
+  fav = prompt("what is your favourite art subject of these (modern, archi, abstract) " + name + "?")
+}
+
+
 
 if (fav == "modern") {
 
-  document.write("<section>" +"<div>" +
+  document.write("<section>" + "<div>" +
     "<img src='https://cdn-anmal.nitrocdn.com/vJgtiwkCsLhvhzZzqhfdngZGCtTVAAGD/assets/static/optimized/rev-25bc62b/wp-content/uploads/2020/03/modern-paintings-art-4-768x768.jpg'/>" +
     "<h4>" + "Modern Art" + "</h4>" +
-    "</div>"+ "</section>")
+    "</div>" + "</section>")
 
 }
 
 else if (fav == "archi") {
 
-  document.write("<section>" +"<div>" +
+  document.write("<section>" + "<div>" +
     "<img src='https://onehundrededition.com/wp-content/uploads/2018/12/PARIS-in-100%C2%BA-The-10-Top-Contemporary-Art-Galleries-8.jpg'/>" +
     "<h4>" + "Architecture" + "</h4>" +
-    "</div>"+ "</section>")
+    "</div>" + "</section>")
 
 }
 
@@ -35,6 +40,22 @@ else {
   alert("sorry we can't help you we do not have this subject")
 }
 
+var answer = prompt(name + " who is your favourite artist ?")
+
+
 document.write("<p>" + "Welcome to Our museum " + name + " fan of artist " + answer + "</p>")
 
+var counterstars = prompt("how many stars would rate us ?" + name)
+while (counterstars > 5) {
+  counterstars = prompt("how many stars would rate us ?" + name)
+}
 
+while (counterstars <= 5) {
+
+  document.write("<div>" + "Thanks for rating us" + "</div>")
+  for (var x = 0; x < counterstars; x++) {
+    document.write("&#11088; ")
+  }
+
+  counterstars++
+}
